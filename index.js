87e3776cf9be5d24c5a0b24b40be0a1ac45c3ca0
Bhavya Dua <dua_bhavya@outlook.com>
2022-10-06 15:25:54 -0400

    commit tree 2b85f6ec34c209dd191067f89747ab96d16e20ab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function superbowlWin(record) {
+    const w = record.find((element, index, array) => element.result === "W");
+
+    if (w !== undefined) return w.year;
+    else return w;
+}
